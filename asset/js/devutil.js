63c2dd9a7419e97bd0857a561be14ef0635af4b2
kanonji <kanonji@localhost>
2011-06-10 15:25:24 +0900

    Add dev util javascript

---
+++ asset/js/devutil.js
@@ -0,0 +1,4 @@
+if (typeof window.console != 'object'){
+    window.console = {log:function(){},debug:function(){},info:function(){},warn:function(){},error:function(){},assert:function(){},dir:function(){},dirxml:function(){},trace:function(){},group:function(){},groupEnd:function(){},time:function(){},timeEnd:function(){},profile:function(){},profileEnd:function(){},count:function(){}};
+}
+console.log = console.debug;
