1be16ec30655e4be3c05ca2146296dc1dff37506
kanonji <kanonji@localhost>
2011-07-23 12:23:33 +0900

    fix for ie8

--- asset/js/devutil.js
+++ asset/js/devutil.js
@@ -1,4 +1,7 @@
 if (typeof window.console != 'object'){
     window.console = {log:function(){},debug:function(){},info:function(){},warn:function(){},error:function(){},assert:function(){},dir:function(){},dirxml:function(){},trace:function(){},group:function(){},groupEnd:function(){},time:function(){},timeEnd:function(){},profile:function(){},profileEnd:function(){},count:function(){}};
 }
+if (typeof console.debug != 'object')
+    console.debug = console.log;
+console.l = console.log;
 console.log = console.debug;
